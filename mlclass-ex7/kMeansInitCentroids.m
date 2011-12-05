7a3bd3adebb91e417c7685441060e20929b86b47
Srinivasan Raguraman <ThoughtWorks@blankslate-sr5977.home>
2011-12-05 02:10:06 -0600

    ex7: initialize centroids randomly

--- mlclass-ex7/kMeansInitCentroids.m
+++ mlclass-ex7/kMeansInitCentroids.m
@@ -14,11 +14,8 @@ centroids = zeros(K, size(X, 2));
 %
 
 
-
-
-
-
-
+randidx = randperm(size(X, 1));
+centroids = X(randidx(1:K), :);
 
 % =============================================================
 
